532f6b843f6d6c1f56df42c6cb2724f08c31ebb2
shao.zhang <shao.zhang@dbappsecurity.com.cn>
2018-02-05 15:15:33 +0800

    头部及左侧导航局部调整

--- src/components/mainContent.vue
+++ src/components/mainContent.vue
@@ -2,36 +2,20 @@
   <el-row class="tac">
     <el-col :span="4">
       <el-menu
-        default-active="1-1"
         class="el-menu-vertical-demo"
         @open="handleOpen"
         @close="handleClose">
         <el-submenu index="1">
           <template slot="title">
             <i class="el-icon-location"></i>
-            <span>导航一</span>
+            <span>内容管理</span>
           </template>
           <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
+            <el-menu-item index="1-1">设备管理</el-menu-item>
+            <el-menu-item index="1-2">设备点检</el-menu-item>
+            <el-menu-item index="1-3">设备维护</el-menu-item>
           </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
         </el-submenu>
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
       </el-menu>
     </el-col>
     <el-col :span="20" class="content">
@@ -61,10 +45,10 @@ export default {
   },
   methods: {
     handleOpen(key, keyPath) {
-      console.log(key, keyPath);
+      //console.log(key, keyPath);
     },
     handleClose(key, keyPath) {
-      console.log(key, keyPath);
+      //console.log(key, keyPath);
     }
   }
 }
@@ -75,5 +59,6 @@ export default {
   min-height: 10rem;
   max-height: 84vh;
   overflow-y: auto;
+  padding: 1rem;
 }
 </style>
--- src/components/topNav.vue
+++ src/components/topNav.vue
@@ -3,39 +3,12 @@
     <div class="logo">
       LOGO
     </div>
-    <ul class="nav-show">
-      <li class="nav-active"><i class="el-icon-edit"></i>内容管理</li>
-      <!--<li><Icon type="ios-keypad" />admin</li>
-      <li><Icon type="ios-analytics"></Icon> 意见建议</li>-->
-    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      activeIndex: '1',
-      activeIndex2: '1'
-    }
-  },
-  props: [
 
-  ],
-  components: {
-
-  },
-  computed: {
-
-  },
-  mounted() {
-
-  },
-  methods: {
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
-    }
-  }
 }
 </script>
 
@@ -55,36 +28,5 @@ export default {
     font-weight: bold;
     color: #fff;
     float: left;
-  }
-  .nav-show{
-    overflow: hidden;
-    float: right;
-    margin-right: 12rem;
-  }
-  .nav-show li{
-    display: inline-block;
-    float: left;
-    width: fit-content;
-    font-size: 1.1rem;
-    color: #eee;
-    margin-right: 2rem;
-    cursor: pointer;
-    font-weight: 500;
-    transition: all .2s ease-in-out;
-  }
-  .nav-show li:hover{
-    color: #fff;
-    transition: all .2s ease-in-out;
-  }
-  .nav-show .nav-active{
-    color: #fff;
-  }
-  .nav-show li:last-child{
-    margin-right: 0;
-  }
-  .nav-show li i{
-    margin-right: .2rem;
-    display: inline-block;
-    vertical-align: middle;
   }
 </style>
